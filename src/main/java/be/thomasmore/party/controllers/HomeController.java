2d05451b4ea6aa71803a0cd444abd424dbefbf9a
brent <brentvc@outlook.com>
2022-02-25 02:22:59 +0100

    Exercise 4.6 Herhaling Thymeleaf Syntax

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
 
@@ -19,5 +23,33 @@ public class HomeController {
         int myCalculatedValue = 52 + 44 * 210;
         model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "about";
+    }
+    @GetMapping("/pay")
+    public String pay(Model model){
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy E");
+        String isWeekend = String.valueOf(today.getDayOfWeek());
+        if (String.valueOf(today.getDayOfWeek()).equals("SATURDAY") || String.valueOf(today.getDayOfWeek()).equals("SUNDAY")) {
+            isWeekend = "Prettig weekend, je hebt het verdiend";
+        }
+        else{
+            isWeekend = "Voor je het weet is het weekend!";
+        }
+        model.addAttribute("test",isWeekend);
+        LocalDate uitersteDatum = today.plusDays(30);
+
+        ZonedDateTime now = ZonedDateTime.now();
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("hh:mm:ss a z");
+
+        String todayTime = formatter2.format(now);
+        model.addAttribute("todaytime", todayTime);
+
+        String todayString = formatter.format(today);
+        String uitersteDatumString = formatter.format(uitersteDatum);
+
+        model.addAttribute("today", todayString);
+        model.addAttribute("uitersteDatum", uitersteDatumString);
+
+        return "pay";
     }
 }
